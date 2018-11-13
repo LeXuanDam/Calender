82695299a478fc647322e20db475d9908bb70abf
dampow <lexuandam96@gmail.com>
2018-11-13 17:40:03 +0700

    create project

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="navbar nav_title" style="border: 0;">
+    <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Calander</span></a>
+</div>
+<div class="clearfix"></div>
+<br/>
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+        <h3>General</h3>
+        <ul class="nav side-menu">
+            <li>
+                <a href="{{ route('home') }}"><i class="fa fa-home"></i> Home </a>
+            </li>
+            <li>
+                <a href="/user"><i class="fa fa-user"></i> User </a>
+            </li>
+            <li>
+                <a href="/group"><i class="fa fa-users"></i> Group </a>
+            </li>
+            <li>
+                <a href="/company"><i class="fa fa-building-o"></i> Company </a>
+            </li>
+        </ul>
+    </div>
+</div>
